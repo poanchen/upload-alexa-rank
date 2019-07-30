aac47381da8a1fceab3153b650133b9e8a0996b8
PoAn (Baron) Chen <chen.baron@hotmail.com>
2019-07-30 00:27:07 -0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+config = {}
+
+config['excelDownloadUrl'] = "urlToYourGoogleSheet"
+config['excelFileName'] = "yourFileName"
+config['excelFileExtension'] = "json"
+config['excelFilePath'] = "absolutePathToYourScript"
+
+# Azure Portal
+config['accountName'] = "azureAccountName"
+config['accountKey'] = "azureAccountKey"
+
+# Azure Blob Storage
+config['containerName'] = "containerName"
